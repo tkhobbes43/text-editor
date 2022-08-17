0f18121a93901646aaf0a09b803d15f00a92f5dc
Tommy Lee <t.k.hobbes43@gmail.com>
2022-08-17 00:07:06 -0500

    put function in database.js completed

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,17 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+// PUT FUNCTION
+export const putDb = async (content) => {
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ jate:  content });
+  const result = await request;  
+  console.log('Data successfully saved to database', result);
+};
 // TODO: Add logic for a method that gets all the content from the database
+// GET FUNCTION
 export const getDb = async () => console.error('getDb not implemented');
 
 initdb();
